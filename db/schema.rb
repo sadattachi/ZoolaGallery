6513e83ec638b7aadeff1455d3441bddd6524eea
sadattachi <nazarii200322@gmail.com>
2022-09-14 20:08:18 +0300

    Add ArtWork model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_14_160632) do
+ActiveRecord::Schema.define(version: 2022_09_14_170616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "art_works", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "attachment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
