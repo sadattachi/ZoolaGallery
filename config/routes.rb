89b77ff96f8d8d51364b3943ce8e2aee83d81d3c
sadattachi <nazarii200322@gmail.com>
2022-09-18 18:13:39 +0300

    Add index route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/artists', to: 'artists#index'
   get '/art_works/new', to: 'art_work#new'
   post '/art_works', to: 'art_work#create'
+  get 'artists/:id/art-works', to: 'art_work#index', as: :artist_art_works
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
