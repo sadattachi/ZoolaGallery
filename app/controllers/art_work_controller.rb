61a9df1ae954e76cc8aea4e16763b285601325b3
sadattachi <nazarii200322@gmail.com>
2022-09-18 17:34:27 +0300

    Generate art_work controller

---
+++ app/controllers/art_work_controller.rb
@@ -0,0 +1,21 @@
+class ArtWorkController < ApplicationController
+  before_action :authenticate_user!, only: %i[new]
+  def new
+    @art_work = current_user.art_works.build
+  end
+
+  def create
+    @art_work = current_user.art_works.build(art_work_params)
+    if @art_work.save
+      redirect_to root_path, notice: 'Art work was successfully added!'
+    else
+      redirect_to root_path, alert: 'Unexpected error occurred!'
+    end
+  end
+
+  private
+
+  def art_work_params
+    params.require(:art_work).permit(:title, :user_id, :attachment)
+  end
+end
