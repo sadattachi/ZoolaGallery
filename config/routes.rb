92910bb12f10893bb2748731e7baeac21c9f6dd1
sadattachi <nazarii200322@gmail.com>
2022-09-18 17:34:48 +0300

    Add routes for art_work controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'page#index'
   get '/artists', to: 'artists#index'
+  get '/art_works/new', to: 'art_work#new'
+  post '/art_works', to: 'art_work#create'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
