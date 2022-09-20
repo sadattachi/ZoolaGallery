d4f005afa11177132a124ecaaad1caf38338cd4a
sadattachi <nazarii200322@gmail.com>
2022-09-20 20:08:09 +0300

    Add nickname to user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env.test?
 
-  User.create({ email: 'nazar@email.com', password: 'testtest', avatar: 'placeholder1.png', description: 'test desc' })
-  User.create({ email: 'ivan@email.com', password: 'testtest', avatar: 'placeholder2.png', description: 'desc test' })
+  User.create({ email: 'nazar@email.com', nickname: 'nazar', password: 'testtest', avatar: 'placeholder1.png',
+                description: 'test desc' })
+  User.create({ email: 'ivan@email.com', nickname: 'ivan', password: 'testtest', avatar: 'placeholder2.png',
+                description: 'desc test' })
 
   ArtWork.create({ user_id: 1, title: 'test img 1', attachment: 'image1.png' })
   ArtWork.create({ user_id: 1, title: 'test img 2', attachment: 'image2.png' })
