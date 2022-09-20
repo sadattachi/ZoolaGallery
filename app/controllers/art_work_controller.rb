e161310b422ee16f04d065fafd5272b4a262ae6d
sadattachi <nazarii200322@gmail.com>
2022-09-20 18:28:13 +0300

    Add delete action

--- app/controllers/art_work_controller.rb
+++ app/controllers/art_work_controller.rb
@@ -1,5 +1,6 @@
 class ArtWorkController < ApplicationController
   before_action :authenticate_user!, only: %i[new]
+  before_action :set_art_work, only: %i[destroy]
 
   def index
     @art_works = ArtWork.where(user_id: params[:id].to_i)
@@ -18,7 +19,18 @@ class ArtWorkController < ApplicationController
     end
   end
 
+  def destroy
+    if current_user.id == @art_work.user_id
+      @art_work.destroy
+      redirect_to artist_art_works_path(current_user), notice: 'Artwork was successfully deleted!'
+    end
+  end
+
   private
+
+  def set_art_work
+    @art_work = ArtWork.find(params[:id])
+  end
 
   def art_work_params
     params.require(:art_work).permit(:title, :user_id, :attachment)
