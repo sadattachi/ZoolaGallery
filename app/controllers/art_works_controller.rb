13e5bad112cd32487aaf6a13914916fd7925fbe8
sadattachi <nazarii200322@gmail.com>
2022-09-20 19:12:39 +0300

    Add edit action and fix routes

---
+++ app/controllers/art_works_controller.rb
@@ -0,0 +1,55 @@
+class ArtWorksController < ApplicationController
+  before_action :authenticate_user!, only: %i[new]
+  before_action :set_art_work, only: %i[update edit destroy check_user]
+  before_action :check_user, only: %i[update edit destroy]
+
+  def index
+    @art_works = ArtWork.where(user_id: params[:id].to_i)
+  end
+
+  def new
+    @art_work = current_user.art_works.build
+  end
+
+  def create
+    @art_work = current_user.art_works.build(art_work_params)
+    if @art_work.save
+      redirect_to root_path, notice: 'Art work was successfully added!'
+    else
+      redirect_to root_path, alert: 'Unexpected error occurred!'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @art_work.update(art_work_params)
+      redirect_to artist_art_works_path(current_user), notice: 'Artwork was successfully updated!'
+    else
+      redirect_to artist_art_works_path(current_user), alert: 'Unexpected error occurred!'
+    end
+  end
+
+  def destroy
+    return unless current_user.id == @art_work.user_id
+
+    @art_work.destroy
+    redirect_to artist_art_works_path(current_user), notice: 'Artwork was successfully deleted!'
+  end
+
+  private
+
+  def set_art_work
+    @art_work = ArtWork.find(params[:id])
+  end
+
+  def art_work_params
+    params.require(:art_work).permit(:title, :user_id, :attachment)
+  end
+
+  def check_user
+    return if current_user.id == @art_work.user_id
+
+    redirect_to root_path, alert: "Cannot update or delete other user's artwork"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'page#index'
   get '/artists', to: 'artists#index'
-  get '/art_works/new', to: 'art_work#new'
-  post '/art_works', to: 'art_work#create'
-  delete '/art_works/:id', to: 'art_work#destroy', as: :delete_art_works
-  get 'artists/:id/art-works', to: 'art_work#index', as: :artist_art_works
+  resources :art_works, only: %i[new edit create update destroy]
+  # get '/art_works/new', to: 'art_work#new'
+  # post '/art_works', to: 'art_work#create', as: :art_work
+  # patch '/art_works', to: 'art_work#update'
+  # get '/art_works/:id/edit', to: 'art_work#edit', as: :edit_art_word
+  # delete '/art_works/:id', to: 'art_work#destroy', as: :delete_art_work
+  get 'artists/:id/art-works', to: 'art_works#index', as: :artist_art_works
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
