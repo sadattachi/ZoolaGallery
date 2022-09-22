67f68147c474e4772ef671331f52c02edb009096
sadattachi <nazarii200322@gmail.com>
2022-09-22 14:42:24 +0300

    Remove recoverable option from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
   mount_uploader :avatar, AvatarUploader
 
   has_many :art_works
